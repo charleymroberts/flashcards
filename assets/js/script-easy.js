e93f5557f0d1ca6042083633c11b42f8e7765941
Charley Roberts <charley.m.roberts@gmail.com>
2022-12-22 20:00:21 +0000

    Remove arrays from script-hard & script-easy files
    Remove the commented out arrays of word pairs from the js files for each version of the game now the array is in its own separate file and working correctly

--- assets/js/script-easy.js
+++ assets/js/script-easy.js
@@ -1,20 +1,3 @@
-//array of questions and answer dictionaries
-
-// let fruitDictionary = [
-//     {lang1: "strawberry", lang2: "la fraise"},
-//     {lang1: "raspberry", lang2: "la framboise"},
-//     {lang1: "peach", lang2: "la pêche"},
-//     {lang1: "blueberry", lang2: "la myrtille"},
-//     {lang1: "apple", lang2: "la pomme"},
-//     {lang1: "pear", lang2: "la poire"},
-//     {lang1: "cherry", lang2: "la cerise"},
-//     {lang1: "grape", lang2: "le raisin"},
-//     {lang1: "plum", lang2: "la prune"},
-//     {lang1: "raisin", lang2: "le raisin sec"},
-//     {lang1: "banana", lang2: "la banane"},
-//     {lang1: "pineapple", lang2: "l'ananas"},
-// ];
-
 // displays game info to user 
 
 let language = "French";
